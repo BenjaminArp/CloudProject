06c5457775a03b0e619221cf431aec1d62f7fd7d
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2022-04-26 15:01:10 +0200

    next steps

---
+++ public/script.js
@@ -0,0 +1,22 @@
+let latestTranslation = ""
+async function translate() {
+    let text = document.getElementById("searchTxt").value;
+    console.log(text)
+    text = text.replace("?", "%3F")
+    let url = "http://localhost:4000/translate/" + text
+    let response = await fetch(url)
+    response = await response.json()
+    latestTranslation = response['translation']
+    document.getElementById('result').innerHTML = response['translation'];
+}
+async function speak() {
+    // Initialize new SpeechSynthesisUtterance object
+    let speech = new SpeechSynthesisUtterance();
+
+    // Set Speech Language
+    speech.lang = "en";
+
+    speech.text = latestTranslation;
+
+    window.speechSynthesis.speak(speech);
+}
