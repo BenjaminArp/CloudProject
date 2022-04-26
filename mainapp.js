ebe1050207965fa75e1256d7029c2e03cfa0f8b0
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2022-04-26 11:23:28 +0200

    first progress

---
+++ mainapp.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(4000);
+
+const bodyParser = require("body-parser");
+
+const translate = require("translate");
+
+translate.engine = "google";
+
+const LanguageDetect = require('languagedetect');
+const lngDetector = new LanguageDetect();
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'))
+
+app.get(`/api`, function (request, response) {
+    response.send('This is version 1.0 of Cloud Project');
+});
+
+
+
+app.get(`/translate/:text`, async (request, response) => {
+    //const text = request.body.text
+    const text = request.params.text
+
+    const posLanguages = lngDetector.detect(text)
+
+    const params = detectLanguage(posLanguages);
+
+
+    const translatedText = await translate(text, params)
+    response.send({"translation" : translatedText});
+});
+
+
+
+function detectLanguage(languageList){
+    for (const posLanguagesKey of languageList) {
+        if (posLanguagesKey[0] == 'german'){
+            return  { to: "en", from: "de"};
+        }
+        else if (posLanguagesKey[0] == 'english'){
+            return { to: "en", from: "en"};
+        }
+        else if (posLanguagesKey[0] == 'french'){
+            return { to: "en", from: "fr"};
+        }
+        else if (posLanguagesKey[0] == 'spanish'){
+            return { to: "en", from: "es"};
+        }
+    }
+    return { to: "en", from: "en"};
+}
