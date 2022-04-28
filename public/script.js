2ed8edf3005b3e0a480ac186be880cf6d13418b0
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2022-04-28 11:02:51 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ let latestTranslation = ""
 async function translateGivenText() {
     let text = document.getElementById("searchTxt").value;
     text = text.replace("?", "%3F")
-    let url = "http://localhost:4000/translate/" + text
+    let url = "http://193.196.54.78:4000/translate/" + text
     let response = await fetch(url)
     response = await response.json()
     latestTranslation = response['translation']
@@ -11,7 +11,7 @@ async function translateGivenText() {
 
 async function speakCustomText() {
     let text = latestTranslation.replace("?", "%3F")
-    let url = "http://localhost:4000/speak/" + text
+    let url = "http://193.196.54.78:4000/speak/" + text
     let response = await fetch(url)
     response = await response.json()
     const snd = new Audio("data:audio/wav;base64," + response['audio']);
